072ded51debfd6985d33b4d97c384f01f102cf7a
Dev Sen <devksen.personal@gmail.com>
2026-01-10 21:28:30 +0530

    add: tuples and sets'

---
+++ 2. Basics/1. Data_Types/15. Tuples.py
@@ -0,0 +1,15 @@
+# Tuple - are like lists but its immutable, we can't change or modify them.
+
+my_tuples = (1,2,3,4,5,6)
+print(5 in my_tuples)
+
+# Methods in Tuple
+
+# count() - how many given elements in tuple
+print(my_tuples.count(5))
+
+# index() - index no. of given element
+print(my_tuples.index(2))
+
+# len() - length of tuple
+print(len(my_tuples))
---
+++ 2. Basics/1. Data_Types/16. Set.py
@@ -0,0 +1,18 @@
+# Set - Unordered collection of unique objects.
+
+my_set = {1, 2, 3, 4, 5, 6, 6}
+
+# return unique values only
+# print(my_set) 
+
+my_set.add(2)
+my_set.add(100)
+# print(my_set)
+
+
+# set() - return the set
+list = [1,2,3,4,4,5,6,7,7,8]
+print(list)
+print(set(list))
+
+# supports copy(), len() methods
