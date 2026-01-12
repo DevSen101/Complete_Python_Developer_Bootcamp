46f75f1fcd9f16cc360ea9243b26c8c521ddc974
Dev Sen <devksen.personal@gmail.com>
2026-01-12 17:06:43 +0530

    learn: scopes concept

---
+++ 2. Basics/5. Functions/8. Scopes.py
@@ -0,0 +1,16 @@
+# Scope = what variables i have access to 
+# Rule
+# (1) start with local
+# (2) parent local
+# (3) Global
+# (4) built in python function
+
+# global keyword
+
+# nonlocal keyword
+
+# why do we need scope
+
+Mu = 'kodfjk'
+Mu.u
+
