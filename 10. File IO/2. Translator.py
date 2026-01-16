4b811fc15b071b29f18041051aeb7d2c4b75cf81
Dev Sen <devksen.personal@gmail.com>
2026-01-16 16:08:51 +0530

    project: create a translator to translate japanese lang

---
+++ 10. File IO/2. Translator.py
@@ -0,0 +1,13 @@
+from translate import Translator
+
+translator = Translator(to_lang="ja")
+
+try:
+    with open("G:/Python Code/10. File IO/test.txt", mode="r") as my_file:
+        text = my_file.read()
+        translation = translator.translate(text)
+        with open("./test-ja.txt", mode="w", encoding='utf-8') as my_file2:
+            my_file2.write(translation)
+
+except FileNotFoundError as e:
+    print("Check your file path silly")
